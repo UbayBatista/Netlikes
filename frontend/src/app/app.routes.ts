d45f72567f1b01e9128dac1006146d8774be3e87
Kilian Santana Delgado <kiliandelgado2113@gmail.com>
2026-04-05 21:27:10 +0100

    feat(frontend): add home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Home } from './pages/home/home';
 
 
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  { path: 'home', component: Home },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 
 ];
