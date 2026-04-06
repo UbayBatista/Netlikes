e40f5d0df6284dc24e3723d92ae45823be756e78
Ángel Sergio Reyes Guedes <asergiorg2005@gmail.com>
2026-04-06 11:15:49 +0100

    feat(backend): add user entity

---
+++ backend/netlikes/src/main/java/software/ulpgc/netlikes/model/User.java
@@ -0,0 +1,24 @@
+package software.ulpgc.netlikes.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+
+@Entity
+@Table(name="user")
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+public class User {
+    @Id private String email;
+    @Column(nullable=false) private String password;
+    @Column(nullable=false) private String securityQuestion;
+    @Column(nullable=false) private String answer;
+    @Column(nullable=false) private String name;
+    @Column(nullable=false) private Date birthdate;
+    @Column(nullable=false) private boolean accountPrivacity;
+    @Column(nullable=false) private boolean watchedFilms;
+    @Column(nullable=false) private boolean filmsToWatchLater;
+    @Column(nullable=false) private boolean recommendedFilms;
+    @Column(nullable=true) private String profilePicture;
+    @Column(nullable=true) private String bio;
+}
---
+++ backend/netlikes/src/main/java/software/ulpgc/netlikes/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package software.ulpgc.netlikes.repository;
+
+import software.ulpgc.netlikes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {}
