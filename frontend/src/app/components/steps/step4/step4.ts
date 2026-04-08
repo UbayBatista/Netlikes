cf501523069ef563f37f8d6caecb6035a8703908
AliciaM05 <aliciarotru@gmail.com>
2026-04-08 12:03:00 +0100

    feat(frontend) add all genres to register

--- frontend/src/app/components/steps/step4/step4.ts
+++ frontend/src/app/components/steps/step4/step4.ts
@@ -13,18 +13,24 @@ export class Step4 {
 
   generos = [
     { name: 'Acción', selected: false },
-    { name: 'Comedia', selected: false },
-    { name: 'Drama', selected: false },
-    { name: 'Terror', selected: false },
-    { name: 'Ciencia Ficción', selected: false },
-    { name: 'Romance', selected: false },
-    { name: 'Documental', selected: false },
-    { name: 'Animación', selected: false },
-    { name: 'Musical', selected: false },
     { name: 'Aventura', selected: false },
-    { name: 'Suspense', selected: false },
+    { name: 'Animación', selected: false },
+    { name: 'Comedia', selected: false },
+    { name: 'Crimen', selected: false },
+    { name: 'Documental', selected: false },
+    { name: 'Drama', selected: false },
+    { name: 'Familia', selected: false },
+    { name: 'Fantasía', selected: false },
     { name: 'Historia', selected: false },
-    { name: 'Bélica', selected: false }
+    { name: 'Terror', selected: false },
+    { name: 'Musical', selected: false },
+    { name: 'Misterio', selected: false },
+    { name: 'Romance', selected: false },
+    { name: 'Ciencia Ficción', selected: false },
+    { name: 'Película de TV', selected: false },
+    { name: 'Suspense', selected: false },
+    { name: 'Bélica', selected: false },
+    { name: 'Western', selected: false }
   ];
 
   get totalSelected(): number {
