54be6233ac01232592f01b839797291133eedf86
Ángel Sergio Reyes Guedes <asergiorg2005@gmail.com>
2026-04-19 23:05:08 +0100

    fix(frontend): removed films without picture of users profile

--- frontend/src/app/pages/profile/profile-body.ts
+++ frontend/src/app/pages/profile/profile-body.ts
@@ -78,26 +78,17 @@ export class ProfileComplete implements OnInit{
         { title: 'Avengers: Endgame', image: 'https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg' },
         { title: 'Avengers: Infinity War', image: 'https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg' },
         { title: 'The Avengers', image: 'https://image.tmdb.org/t/p/w500/aMETsaNNcDc6g5ZatQtVbySnSaA.jpg' },
-        { title: 'Captain America: Civil War', image: 'https://image.tmdb.org/t/p/w500/5N20rQURev5CNDcMjHVUZhpoCNC.jpg' },
         { title: 'Deadpool 2', image: 'https://image.tmdb.org/t/p/w500/qjiPP4FhTV3UAGa1Dbf2qEqTvu5.jpg' },
         { title: 'Shazam!', image: 'https://image.tmdb.org/t/p/w500/yUOJHa9XmB1H0iYodG9Kb3YCc9T.jpg' },
-        { title: 'Star Wars: The Force Awakens', image: 'https://image.tmdb.org/t/p/w500/fYzpM9GmpBlIC893fNjoWCwE24H.jpg' },
-        { title: 'Minions', image: 'https://image.tmdb.org/t/p/w500/s5uMY8ooGRZOL0oe4sIvnlTsYQO.jpg' },
         { title: 'Spectre (007)', image: 'https://image.tmdb.org/t/p/w500/mSvpKOWbyFtLro9BjfEGqUw5dXE.jpg' },
         { title: 'Fight Club', image: 'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg' }
     ];
 
     @Input() filmsLater: any[] = [
-        { title: 'Interstellar', image: 'https://image.tmdb.org/t/p/w500/gEU2QvYBfw7fg7vgoYbxNSR76sR.jpg' },
         { title: 'Inception', image: 'https://image.tmdb.org/t/p/w500/oYuLEt3zVCKq57qu2F8dT7NIa6f.jpg' },
-        { title: 'The Matrix', image: 'https://image.tmdb.org/t/p/w500/f89U3Y9L9UnpgH3p9Szu9YQG2fv.jpg' },
         { title: 'The Dark Knight', image: 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg' },
-        { title: 'Avatar', image: 'https://image.tmdb.org/t/p/w500/kyeqWdyKINLSYWZWTcgmAgleExn.jpg' },
-        { title: 'Star Wars: A New Hope', image: 'https://image.tmdb.org/t/p/w500/6FfCtvVAxD9pm97N10o9Szu9YQG.jpg' },
-        { title: 'Star Wars: The Force Awakens', image: 'https://image.tmdb.org/t/p/w500/fYzpM9GmpBlIC893fNjoWCwE24H.jpg' },
         { title: 'Venom', image: 'https://image.tmdb.org/t/p/w500/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg' },
-        { title: 'Mad Max: Fury Road', image: 'https://image.tmdb.org/t/p/w500/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg' },
-        { title: 'The Martian', image: 'https://image.tmdb.org/t/p/w500/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg' }
+        { title: 'Mad Max: Fury Road', image: 'https://image.tmdb.org/t/p/w500/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg' }
     ];
 
     @Input() filmsViewOther: any[] = [
@@ -108,42 +99,17 @@ export class ProfileComplete implements OnInit{
         { title: 'Titanic', image: 'https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg' },
         { title: 'Joker', image: 'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg' },
         { title: 'Parasite', image: 'https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg' },
-        { title: 'Pulp Fiction', image: 'https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPbOYKQru1U.jpg' },
-        { title: 'Gladiator', image: 'https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvq6.jpg' },
         { title: 'The Lord of the Rings', image: 'https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg' }
     ];
 
     @Input() filmsLaterOther: any[] = [
-        { title: 'Toy Story', image: 'https://image.tmdb.org/t/p/w500/rhIRbceoE9lR4veEXuwCC2wARtG.jpg' },
-        { title: 'Spirited Away', image: 'https://image.tmdb.org/t/p/w500/39wmItIWsg5sZMyRUHLkPcrvz2m.jpg' },
         { title: 'Spider-Man: Into the Spider-Verse', image: 'https://image.tmdb.org/t/p/w500/iiZZdoQBEYBv6id8su7ImL0oCbD.jpg' },
-        { title: 'Coco', image: 'https://image.tmdb.org/t/p/w500/gGEsBPAijhVMRkvq5K11bAIn2X2.jpg' },
         { title: 'Up', image: 'https://image.tmdb.org/t/p/w500/vpbaStTMt8qqXaEgnOR2EE4DNJk.jpg' },
-        { title: 'Inside Out', image: 'https://image.tmdb.org/t/p/w500/lRHE0vzf30Cv10fAexP0OQkRwwZ.jpg' },
-        { title: 'Wall-E', image: 'https://image.tmdb.org/t/p/w500/hbhFnRcwDkX1cT124T0rN5V6kF6.jpg' },
-        { title: 'The Lion King', image: 'https://image.tmdb.org/t/p/w500/sKCr78MXSLixwmZ8DyJLreANYu.jpg' },
-        { title: 'Shrek', image: 'https://image.tmdb.org/t/p/w500/iB64vpL3dIObYkFEkxcYQcpK5I3.jpg' },
-        { title: 'Minions', image: 'https://image.tmdb.org/t/p/w500/s5uMY8ooGRZOL0oe4sIvnlTsYQO.jpg' },
         { title: 'Avengers: Endgame', image: 'https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg' },
         { title: 'Avengers: Infinity War', image: 'https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg' },
         { title: 'The Avengers', image: 'https://image.tmdb.org/t/p/w500/aMETsaNNcDc6g5ZatQtVbySnSaA.jpg' },
-        { title: 'Captain America: Civil War', image: 'https://image.tmdb.org/t/p/w500/5N20rQURev5CNDcMjHVUZhpoCNC.jpg' },
         { title: 'Deadpool 2', image: 'https://image.tmdb.org/t/p/w500/qjiPP4FhTV3UAGa1Dbf2qEqTvu5.jpg' },
-        { title: 'Shazam!', image: 'https://image.tmdb.org/t/p/w500/yUOJHa9XmB1H0iYodG9Kb3YCc9T.jpg' },
-        { title: 'Star Wars: The Force Awakens', image: 'https://image.tmdb.org/t/p/w500/fYzpM9GmpBlIC893fNjoWCwE24H.jpg' },
-        { title: 'Minions', image: 'https://image.tmdb.org/t/p/w500/s5uMY8ooGRZOL0oe4sIvnlTsYQO.jpg' },
-        { title: 'Spectre (007)', image: 'https://image.tmdb.org/t/p/w500/mSvpKOWbyFtLro9BjfEGqUw5dXE.jpg' },
-        { title: 'Fight Club', image: 'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg' },
-        { title: 'Avengers: Endgame', image: 'https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg' },
-        { title: 'Avengers: Infinity War', image: 'https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg' },
-        { title: 'The Avengers', image: 'https://image.tmdb.org/t/p/w500/aMETsaNNcDc6g5ZatQtVbySnSaA.jpg' },
-        { title: 'Captain America: Civil War', image: 'https://image.tmdb.org/t/p/w500/5N20rQURev5CNDcMjHVUZhpoCNC.jpg' },
-        { title: 'Deadpool 2', image: 'https://image.tmdb.org/t/p/w500/qjiPP4FhTV3UAGa1Dbf2qEqTvu5.jpg' },
-        { title: 'Shazam!', image: 'https://image.tmdb.org/t/p/w500/yUOJHa9XmB1H0iYodG9Kb3YCc9T.jpg' },
-        { title: 'Star Wars: The Force Awakens', image: 'https://image.tmdb.org/t/p/w500/fYzpM9GmpBlIC893fNjoWCwE24H.jpg' },
-        { title: 'Minions', image: 'https://image.tmdb.org/t/p/w500/s5uMY8ooGRZOL0oe4sIvnlTsYQO.jpg' },
-        { title: 'Spectre (007)', image: 'https://image.tmdb.org/t/p/w500/mSvpKOWbyFtLro9BjfEGqUw5dXE.jpg' },
-        { title: 'Fight Club', image: 'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg' }
+        { title: 'Shazam!', image: 'https://image.tmdb.org/t/p/w500/yUOJHa9XmB1H0iYodG9Kb3YCc9T.jpg' }
     ];
 
     canScrollLeft = false;
