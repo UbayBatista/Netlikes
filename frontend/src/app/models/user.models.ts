54d0a23d299d450bdb9190df61899674f08ac0fa
Kilian Santana Delgado <kiliandelgado2113@gmail.com>
2026-04-20 20:37:17 +0100

    feat(frontend): create user models

---
+++ frontend/src/app/models/user.models.ts
@@ -0,0 +1,21 @@
+export interface Credentials {
+    email: string;
+    password: string;
+}
+
+export interface User {
+    name: string;
+    email: string;
+    profilePicture: string;
+}
+
+export interface RegisterData{
+    userName: string;
+    email: string;
+    birthdate: string;
+    password: string;
+    securityQuestion: string;
+    answer: string;
+    favoriteGenres: {id: number, genre: string}[];
+}
+
